feda5e0f5b915c81325c163dedb85c82c725b3b9
Oskar Woźniak <oskar.tomasz.wozniak@gmail.com>
2021-06-06 14:45:14 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public todos: string[] = [];
+  public todoForm = new FormGroup({
+    todo: new FormControl('', Validators.required)
+  });
+  public onSubmit(): void{
+    const newTodo = this.todoForm.value.todo;
+    if(newTodo !== ''){
+    this.todos.push(newTodo);
+    this.todoForm.setValue({
+      todo: ''
+    })
+  }
+  }
+}
